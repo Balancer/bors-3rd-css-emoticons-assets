f09c8fc1372ac272084ca01b84748e466491e9ab
Balancer <balancer@balancer.ru>
2014-01-10 15:48:02 +0400

    Унификация.

--- classes/jquery/cssemoticons.php
+++ classes/jquery/cssemoticons.php
@@ -8,40 +8,9 @@ class jquery_cssemoticons
 		<script src="javascripts/jquery.cssemoticons.js" type="text/javascript"></script>
 	*/
 
-	static function load()
-	{
-		jquery::load();
-
-		$loader = \AssetsManager\Loader::getInstance();
-		$package = $loader->getPackage('balancer/bors-3rd-css-emoticons-assets');
-		$preset = $loader->getPreset('jquery.cssemoticons');
-		foreach($preset->getOrganizedStatements() as $type => $statements)
-		{
-			foreach ($statements as $statement)
-			{
-				$path = str_replace('../www/vendor', '/_bors-assets', $statement->__toString());
-				switch($type)
-				{
-					case 'css':
-						bors_use($path);
-						break;
-					case 'jsfiles_footer':
-						jquery::plugin($path);
-						break;
-					default:
-						bors_throw('Unknown type '.$type);
-				}
-			}
-		}
-
-//		jquery::plugin_css('cssemoticons');
-//		jquery::plugin('cssemoticons');
-//		$html = 
-	}
-
 	static function appear($element, $params = array())
 	{
-		self::load();
+		self::load('cssemoticons', 'bors-3rd-css-emoticons-assets');
 		jquery::appear($element, 'emoticonize', $params);
 	}
 }
